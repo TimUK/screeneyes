38f770aeee7de30c68b3c079a016d78f6af64eb8
TimUK <timj178@gmail.com>
2020-05-03 21:50:44 +0100

    Added timer logic & basic alerting

--- ScreenEyes/Form1.Designer.cs
+++ ScreenEyes/Form1.Designer.cs
@@ -29,11 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 52);
+            this.label1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Work Time Left:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label3.Location = new System.Drawing.Point(99, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 44);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BREAK TIME";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(477, 256);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Screen Eyes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ScreenEyes/Form1.cs
+++ ScreenEyes/Form1.cs
@@ -12,9 +12,72 @@ namespace ScreenEyes
 {
     public partial class Form1 : Form
     {
+
+        int SETTING_WORK_TIME_LENGTH = 10;
+        int SETTING_BREAK_TIME_LENGTH = 5;
+
+        int workTime;
+
+        int breakTime;
+
+        Boolean working = true;
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            workTime = SETTING_WORK_TIME_LENGTH;
+
+            breakTime = SETTING_BREAK_TIME_LENGTH;           
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts;
+            if (working)
+            {
+                workTime--;
+                label2.Text = "Work Time Left:";
+                label2.ForeColor = SystemColors.HotTrack;
+                ts = TimeSpan.FromSeconds(workTime);
+                
+                if (workTime == 0)
+                {
+                    breakTime = SETTING_BREAK_TIME_LENGTH;
+                    working = false;
+                    //MessageBox.Show("Work time is over!!");
+                    label3.Visible = true;
+                    this.Activate();
+                }
+            }
+            else
+            {
+                breakTime--;
+                label2.Text = "Break Time Left:";
+                label2.ForeColor = Color.Crimson;
+                ts = TimeSpan.FromSeconds(breakTime);
+                if (breakTime == 0)
+                {
+                    label3.Visible = false;
+                    workTime = SETTING_WORK_TIME_LENGTH;
+                    working = true;
+                    this.Activate();
+                }
+            }
+
+            label1.Text = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
+                ts.Hours,
+                ts.Minutes,
+                ts.Seconds);
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - 495, Screen.PrimaryScreen.Bounds.Height - 303);
         }
     }
 }
